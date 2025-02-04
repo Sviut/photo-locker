b46b1aab0f79fe59e5246e064a88e8dac25be48f
Sviut <jeleznov88@gmail.com>
2025-02-04 10:19:33 +0300

    pass data to FAQ template

--- controllers/static.go
+++ controllers/static.go
@@ -11,3 +11,17 @@ func StaticHandler(tpl views.Template) http.HandlerFunc {
 		tpl.Execute(w, nil)
 	}
 }
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   string
+	}{
+		{"Question 1 ?", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, minima."},
+		{"Question 2 ?", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, minima."},
+		{"Question 3 ?", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, minima."},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
+	}
+}
