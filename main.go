4417f78b334ac53bf5e1617d9d6da7c7db83fdcb
Sviut <jeleznov88@gmail.com>
2025-02-09 11:41:51 +0300

    add sign in template

--- main.go
+++ main.go
@@ -46,7 +46,12 @@ func main() {
 		templates.FS,
 		"signup.gohtml", "tailwind.gohtml",
 	))
+	usersC.Templates.SignIn = views.Must(views.ParseFS(
+		templates.FS,
+		"signin.gohtml", "tailwind.gohtml",
+	))
 	r.Get("/signup", usersC.New)
+	r.Get("/signin", usersC.SignIn)
 	r.Post("/users", usersC.Create)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
