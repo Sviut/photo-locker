2485a79472d54dfad3e31478e2cdde896c07a9bf
Sviut <jeleznov88@gmail.com>
2025-02-12 09:57:58 +0300

    add session handlers

--- models/session.go
+++ models/session.go
@@ -1,7 +1,23 @@
 package models
 
+import "database/sql"
+
 type Session struct {
-	ID        int
-	UserId    int
+	ID     int
+	UserId int
+	// only set when creating a new session
+	Token     string
 	TokenHash string
+}
+
+type SessionService struct {
+	DB *sql.DB
+}
+
+func (ss *SessionService) Create(userId int) (*Session, error) {
+	return nil, nil
+}
+
+func (ss *SessionService) User(token string) (*User, error) {
+	return nil, nil
 }
